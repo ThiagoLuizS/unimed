34cbf36e8f08d0aed8d25b09fdba62ffabe891dc
Meta PetShop <thiagoluiz_bh@hotmail.com>
2025-01-18 17:44:11 -0300

    Initial Commit

---
+++ src/main/java/br/com/seguro/unimed/UnimedApplication.java
@@ -0,0 +1,13 @@
+package br.com.seguro.unimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimedApplication.class, args);
+	}
+
+}
